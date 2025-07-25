4d94b6d84a3cec127e8d7ee2aa2901c062782248
Aaron Tsang <aarontsang@Aarons-MacBook-Air.local>
2025-07-24 20:12:52 -0700

    updated website design + information

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 window.onload = () => {
     const element = document.getElementById("typed-name");
-    const message = "Hello, my name is Aaron. Welcome to my website!";
+    const message = "Hi, I'm Aaron Tsang";
     let index = 0;
 
     function type() {
         if (index < message.length) {
             element.textContent += message[index++];
-            setTimeout(type, 75); // adjust speed here
+            setTimeout(type, 100); // adjust speed here
         }
     }
 
